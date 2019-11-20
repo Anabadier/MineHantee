5d6a6f68bccb336df3fa99fe67102e6226056987
LittleCoinCoin <56084809+LittleCoinCoin@users.noreply.github.com>
2019-11-20 18:13:42 +0100

    Mise en place du lancement du jeu
    Le jeu commencera avec ces fenêtres. Ce sera à lier avec la mise en place du réseau puis l'instanciation de PyGame pour le plateau du jeu

--- SaC.py
+++ SaC.py
@@ -4,20 +4,25 @@ Created on Fri Nov 15 14:28:20 2019
 
 @author: eliot
 """
-import os
-import tkinter as tk
+
 from tkinter import filedialog as tkfd
 
  
 class Save_and_Charge(object):
-    def __init__(self, _scalers):
+    def __init__(self, _scalers, _ref_launcher):
         self.scalers = _scalers
+        self.ref_launcher = _ref_launcher
     
-    def setObjectValues(self, _list_Objects, _list_values):
+    def setScalersValues(self, _list_values):
         l = len(_list_values)
         try:
-            for k in range(l):
-                _list_Objects[k].set(_list_values[k])
+            self.scalers[0].config(state="normal")
+            self.scalers[0].delete(0,'end')
+            self.scalers[0].insert(1,_list_values[0])
+            self.scalers[0].config(state="readonly")
+            for k in range(1,l):
+                self.scalers[k].set(_list_values[k])
+            self.ref_launcher.update_nb_fantome_max()
             print("Les valeurs ont été chargées.")
         except IndexError:
             print("Un problème est survenu lors du chargement des valeurs.")
@@ -40,7 +45,7 @@ class Save_and_Charge(object):
         if (file_path != ""):
             content = self.read_file(_file_path=file_path)
             values = [val.split(",")[1] for val in content]
-            self.setObjectValues(self.scalers, values)
+            self.setScalersValues(values)
     
     def save_config_file(self):
         values = self.getObjectValues(self.scalers)
@@ -68,114 +73,4 @@ class Save_and_Charge(object):
     
     def save_game_file(self):
         print("in save_config_file")
-
-if __name__=="__main__":
-    
-    def first_window():
-        
-        PartieNormale_Button = tk.Button(master=fen,
-                                        text = "Jouer à une partie standard",
-                                        padx=10)
-                                        #command = )
-        PartieNormale_Button.grid(column=0, row=0) 
-        
-        ChargeConfig_Button = tk.Button(master=fen,
-                                        text = "Configurer une partie",
-                                        padx=10,
-                                        command = configuration_window)
-        ChargeConfig_Button.grid(column=1, row=0)
-        
-    
-    def configuration_window():
-        global fen
-        
-        fen.destroy()
-        fen = tk.Tk(className = " Configuration de la Mine")
-        
-        Scale_DimPlateau = tk.Scale(master = fen,
-                                    orient = "horizontal",
-                                    label = "Dimension du plateau",
-                                    from_ = 7, to = 21, resolution = 2,
-                                    width = 20, length = 500,
-                                    activebackground = "#105105105",
-                                    relief = "sunken")
-        Scale_DimPlateau.grid(column=0, row=0)
-        
-        Scale_NbJoueur = tk.Scale(master = fen,
-                                    orient = "horizontal",
-                                    label = "Nombre de Joueurs",
-                                    from_ = 2, to = 4, resolution = 1,
-                                    width = 20, length = 500,
-                                    activebackground = "#105105105",
-                                    relief = "sunken")
-        Scale_NbJoueur.grid(column=1, row=0)
-        
-        Scale_NbFantomeOdM = tk.Scale(master = fen,
-                                    orient = "horizontal",
-                                    label = "Nombre de Fantômes sur l'ordre de mission",
-                                    from_ = 1, to = 21, resolution = 1,
-                                    width = 20, length = 500,
-                                    activebackground = "#105105105",
-                                    relief = "sunken")
-        Scale_NbFantomeOdM.grid(column=0, row=1, columnspan = 2)
-        
-        Scale_PtsPepite = tk.Scale(master = fen,
-                                    orient = "horizontal",
-                                    label = "Points pour 1 pépite d'or",
-                                    from_ = 1, to = 100, resolution = 1,
-                                    tickinterval = 10,
-                                    width = 20, length = 500,
-                                    activebackground = "#105105105",
-                                    relief = "sunken")
-        Scale_PtsPepite.grid(column=0, row=2, columnspan = 2)
-        
-        Scale_PtsFantome = tk.Scale(master = fen,
-                                    orient = "horizontal",
-                                    label = "Points pour 1 fantôme",
-                                    from_ = 1, to = 100, resolution = 1,
-                                    tickinterval = 10,
-                                    width = 20, length = 500,
-                                    activebackground = "#105105105",
-                                    relief = "sunken")
-        Scale_PtsFantome.grid(column=0, row=3, columnspan = 2)
-        
-        Scale_PtsFantomeOdM = tk.Scale(master = fen,
-                                    orient = "horizontal",
-                                    label = "Points pour 1 fantôme de l'ordre de mission",
-                                    from_ = 1, to = 100, resolution = 1,
-                                    tickinterval = 10,
-                                    width = 20, length = 500,
-                                    activebackground = "#105105105",
-                                    relief = "sunken")
-        Scale_PtsFantomeOdM.grid(column=0, row=4, columnspan = 2)
-        
-        
-        scalers = [Scale_DimPlateau, Scale_NbJoueur, Scale_NbFantomeOdM,
-                   Scale_PtsPepite, Scale_PtsFantome, Scale_PtsFantomeOdM]
-        SaverCharger = Save_and_Charge(scalers)
-        values = SaverCharger.read_file(_file_path = os.getcwd()+"/config.csv")
-        values = [val.split(",")[1] for val in values]
-        SaverCharger.setObjectValues(scalers, values)
-        
-        ChargeConfig_Button = tk.Button(master=fen,
-                                        text = "Charger une configuration",
-                                        padx=10,
-                                        command = SaverCharger.charge_config_file)
-        ChargeConfig_Button.grid(column=0, row=10)
-        
-        SaveConfig_Button = tk.Button(master=fen,
-                                        text = "Sauvegarder une configuration",
-                                        padx=10,
-                                        command = SaverCharger.save_config_file)
-        SaveConfig_Button.grid(column=1, row=10)
-        
-        fen.mainloop()
-    
-    
-    
-    fen = tk.Tk(className = " La Mine Hantée")
-    
-    first_window()
-    
-    fen.mainloop()
     
